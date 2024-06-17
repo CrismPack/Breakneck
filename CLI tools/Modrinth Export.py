e7c67440d415d37be4350409a112822a1406aeef
HaXr <haxr@outlook.dk>
2024-06-18 01:22:43 +0200

    packwiz-installer

--- CLI tools/Modrinth Export.py
+++ CLI tools/Modrinth Export.py
@@ -13,10 +13,13 @@ import toml  # pip install toml
 git_path = "D:\\GitHub Projects\\Breakneck\\"
 minecraft_version = "1.21"
 packwiz_path = git_path + "Packwiz\\" + minecraft_version + "\\"
-#packwiz_exe_path = os.path.join("..", "packwiz.exe")
-packwiz_exe_path = os.path.expanduser("~") + "\\go\\bin\\packwiz.exe"
 
-# cf_zip_path = ""
+packwiz_exe_path = os.path.expanduser("~") + "\\go\\bin\\packwiz.exe"
+packwiz_manifest = "pack.toml"
+
+packwiz_installer_path = git_path + "CLI tools\\packwiz-installer-bootstrap.jar"
+# print("[DEBUG] " + packwiz_installer_path)
+
 pack_version = "4.0.0_pre2"
 
 refresh_only = False
@@ -31,25 +34,35 @@ def main():
     # Export Modrinth pack and manifest via mmc-export method
     if mmc_export_modrinth_export and not refresh_only:
         
-        subprocess.call(f"{packwiz_exe_path} cf export", shell=True)
+
+        # Export CF modpack.
+        # subprocess.call(f"{packwiz_exe_path} cf export", shell=True)
         cf_zip = f"Breakneck-{minecraft_version}-{pack_version}.zip"
-        print("[Debug]: " + cf_zip)
+        #print("[Debug]: " + cf_zip)
         
-        #mmc_zip_root = str(Path(cf_zip).parents[0])
+        mmc_zip_root = str(Path(cf_zip).parents[0])
         #print("[Debug]: " + mmc_zip_root)
         
         mmc_config = git_path + "Packwiz\\mmc-export.toml"
         
+
+        print("[DEBUG] " + packwiz_installer_path)
+
+        subprocess.call(f"java -jar \"{packwiz_installer_path}\" {packwiz_manifest}", shell=True)
+
+
+
+
         args = (
             "mmc-export",
-            "--input", "Breakneck-1.21-4.0.0_pre2.zip",#cf_zip,
+            "--input", cf_zip,
             "--format", "Modrinth",
             "--modrinth-search", "loose",
-            #"-o", "\\",
+            "-o", mmc_zip_root,
             "-c", mmc_config,
             "-v", pack_version,
             "--scheme", "{name}-{version}",
-        ); subprocess.call(args, shell=True)
+        ); #subprocess.call(args, shell=True)
     
     elif refresh_only:
         subprocess.call(f"{packwiz_exe_path} refresh", shell=True)
