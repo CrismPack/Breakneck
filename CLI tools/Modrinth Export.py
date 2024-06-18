c6dfd60718da55b271a6bb630bf08e763dec7464
HaXr <haxr@outlook.dk>
2024-06-18 04:15:52 +0200

    It's alive!
    (Fixed the implementation of MMC exporting to successfully convert from Packwiz to Modrinth)

--- CLI tools/Modrinth Export.py
+++ CLI tools/Modrinth Export.py
@@ -4,25 +4,25 @@ import subprocess
 from typing import IO
 from pathlib import Path
 from zipfile import ZipFile
-from shutil import unpack_archive
+from shutil import unpack_archive, rmtree, make_archive
 
 import toml  # pip install toml
 
 
 #user_path = os.path.expanduser("D:")
+modpack_name = "Breakneck"
 git_path = "D:\\GitHub Projects\\Breakneck\\"
 minecraft_version = "1.21"
-packwiz_path = git_path + "Packwiz\\" + minecraft_version + "\\"
-
-packwiz_exe_path = os.path.expanduser("~") + "\\go\\bin\\packwiz.exe"
-packwiz_manifest = "pack.toml"
-
-packwiz_installer_path = git_path + "CLI tools\\packwiz-installer-bootstrap.jar"
-# print("[DEBUG] " + packwiz_installer_path)
-
+pack_version = "4.0.0_pre2"
 packwiz_side = "client"
 
-pack_version = "4.0.0_pre2"
+
+
+packwiz_path = git_path + "Packwiz\\" + minecraft_version + "\\"
+packwiz_exe_path = os.path.expanduser("~") + "\\go\\bin\\packwiz.exe"
+packwiz_manifest = "pack.toml"
+packwiz_installer_path = git_path + "CLI tools\\packwiz-installer-bootstrap.jar"
+
 
 refresh_only = False
 mmc_export_modrinth_export = True
@@ -40,31 +40,63 @@ def main():
         # Export CF modpack.
         # subprocess.call(f"{packwiz_exe_path} cf export", shell=True)
         cf_zip = f"Breakneck-{minecraft_version}-{pack_version}.zip"
-        #print("[Debug]: " + cf_zip)
         
         mmc_zip_root = str(Path(cf_zip).parents[0])
-        #print("[Debug]: " + mmc_zip_root)
         
         mmc_config = git_path + "Packwiz\\mmc-export.toml"
         
 
-        print("[DEBUG] " + packwiz_installer_path)
-
-        subprocess.call(f"java -jar \"{packwiz_installer_path}\" -s {packwiz_side} {packwiz_manifest}", shell=True)
 
 
+        # Export Packwiz modpack to MMC cache folder and zip it.
+        mmc_cache_path = packwiz_path + "mmc-cache\\"
+        # try:
+        #     rmtree(mmc_cache_path)
+        # except:
+        #     print("")
+        
+        try:
+            os.mkdir(mmc_cache_path)
+        except:
+            print("")
 
+        os.chdir(mmc_cache_path)
+
+        retain = ["packwiz-installer.jar", "mmc-pack.json", "instance.cfg"] # Files that shouldn't be deleted
+        
+        # Loop through everything in folder in current working directory
+        for item in os.listdir(os.getcwd()):
+            if item not in retain:  # If it isn't in the list for retaining
+                try:
+                    os.remove(item)  # Remove the item
+                except OSError as e:
+                    print(e)
+
+                try:
+                    rmtree(item)
+                except OSError as e:
+                    print(e)
+
+
+        subprocess.call(f"java -jar \"{packwiz_installer_path}\" -s {packwiz_side} \"{packwiz_path + packwiz_manifest}\"", shell=True)
+        make_archive("mcc-cache", 'zip', mmc_cache_path)
+
+
+        os.chdir(packwiz_path)
+
+
+        # subprocess.call("mmc-export gh-login")
 
         args = (
             "mmc-export",
-            "--input", cf_zip,
+            "--input", mmc_cache_path + "mcc-cache.zip",#"mcc-cache.zip",#mmc_cache_path,
             "--format", "Modrinth",
             "--modrinth-search", "loose",
-            "-o", mmc_zip_root,
+            "-o", packwiz_path,
             "-c", mmc_config,
             "-v", pack_version,
-            "--scheme", "{name}-{version}",
-        ); #subprocess.call(args, shell=True)
+            "--scheme", modpack_name + "-{version}",
+        ); subprocess.call(args, shell=True)
     
     elif refresh_only:
         subprocess.call(f"{packwiz_exe_path} refresh", shell=True)
---
+++ Packwiz/1.21/bundled_links.md
@@ -0,0 +1,10 @@
+Sources for bundled mods:
+
+GitHub links
+* [CleanView (Fabric)](https://github.com/zlainsama/CleanView)
+* [Clear Despawn [Fabric]](https://github.com/StrikerRockers-Mods/ClearDespawn-Fabric)
+* [Roughly Enough Professions (REP)](https://github.com/Mrbysco/RoughlyEnoughProfessions)
+* [Shulker Box Tooltip [Fabric/Forge]](https://github.com/MisterPeModder/ShulkerBoxTooltip)
+* [True Darkness [Fabric] - Updated Fork](https://github.com/HaXrBOT/True-Darkness-Fork)
+
+Always check the licenses to see if they allow distribution!
